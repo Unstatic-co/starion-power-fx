e96a55055b2aa5cea1aaa3b3567554b8f7817793
Pham Van Manh <manhpv@starion.co>
2024-06-25 11:59:09 +0700

    feat: add script to upload to r2

---
+++ scripts/upload.js
@@ -0,0 +1,145 @@
+import fs from "fs";
+import path from "path";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { gzipSync } from "zlib";
+import mime from "mime";
+import PQueue from "p-queue";
+import { argv } from "process";
+
+const basePath = "dist";
+const version = argv[2] || "1.0.0";
+const s3Endpoint = argv[3];
+const s3Bucket = argv[4];
+const s3AccessKey = argv[5];
+const s3Secret = argv[6];
+
+/**
+ *
+ * @param {string} path
+ * @returns {string}
+ */
+function normalizePathToFileName(path) {
+	return path.replace(/\\/g, "/").replace(/\//g, "_");
+}
+
+/**
+ *
+ * @param {string} version
+ * @param {string} path
+ * @returns {string}
+ */
+function computeFileName(version, path) {
+	const normalizedPath = normalizePathToFileName(path);
+	return `${version}${normalizedPath}`;
+}
+
+/**
+ *
+ * @param {string} filePath
+ * @returns {Buffer | undefined}
+ */
+function getFileContent(filePath) {
+	try {
+		return fs.readFileSync(filePath);
+	} catch (error) {
+		return undefined;
+	}
+}
+
+/**
+ *
+ * @param {S3Client} client
+ * @param {string} bucket
+ * @param {string} fileName
+ * @param {Buffer} fileContent
+ */
+async function uploadFile(client, bucket, fileName, fileContent) {
+	const command = new PutObjectCommand({
+		ContentEncoding: "gzip",
+		Bucket: bucket,
+		Key: fileName,
+		ContentType: mime.getType(fileName),
+		Body: gzipSync(fileContent),
+	});
+	console.log(`Uploading file ${fileName}`);
+	await client.send(command);
+}
+
+/**
+ *
+ * @param {string} basePath
+ * @returns {fs.Dirent[]}
+ */
+function getAllFiles(basePath) {
+	const files = fs.readdirSync(basePath, {
+		withFileTypes: true,
+		recursive: true,
+	});
+
+	return files;
+}
+
+function uploadFiles() {
+	const folderPath = path.resolve(process.cwd(), basePath);
+	const files = getAllFiles(folderPath);
+	const client = new S3Client({
+		region: "auto",
+		endpoint: s3Endpoint,
+		credentials: {
+			accessKeyId: s3AccessKey,
+			secretAccessKey: s3Secret,
+		},
+	});
+
+	const queue = new PQueue({
+		concurrency: 20,
+	});
+
+	files.forEach((file) => {
+		if (file.isFile()) {
+			const fileContent = getFileContent(path.resolve(file.path, file.name));
+
+			if (fileContent) {
+				queue.add(() =>
+					uploadFile(
+						client,
+						s3Bucket,
+						computeFileName(
+							version,
+							path.resolve(file.path.replace(folderPath, ""), file.name)
+						),
+						fileContent
+					).catch((error) => {
+						console.error(`Error uploading file ${file.name}: ${error}`);
+					})
+				);
+			} else {
+				console.log(`File ${file.name} not found`);
+			}
+		}
+	});
+	queue.start();
+}
+
+uploadFiles();
+
+// const client = new S3Client({
+// 	region: "auto",
+// 	endpoint: s3Endpoint,
+// 	credentials: {
+// 		accessKeyId: s3AccessKey,
+// 		secretAccessKey: s3Secret,
+// 	},
+// });
+
+// uploadFile(
+// 	client,
+// 	s3Bucket,
+// 	computeFileName(
+// 		version,
+// 		"/7/_framework/blazor.webassembly.js"
+// 	),
+// 	fs.readFileSync(
+// 		path.resolve(process.cwd(), "./dist/7/_framework/blazor.webassembly.js")
+// 	)
+// );
